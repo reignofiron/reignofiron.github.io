e04b47eef15b821c5655a45ecbdb94705d69e4f8
Jesse Rogers <jesse@jesserogers.co>
2018-03-18 18:59:02 -0700

    Reset API keys
    oops

--- _site/assets/js/player.js
+++ _site/assets/js/player.js
@@ -1,8 +1,8 @@
 $(function() {
 
   var
-    // apiKey = "39424dade4d141af9a0807725a14ed20", // production
-    apiKey = "6987280b74b24575a4e805277bb5baa6", // local
+    apiKey = "39424dade4d141af9a0807725a14ed20", // production
+    // apiKey = "6987280b74b24575a4e805277bb5baa6", // local
     groupID = "2974952",
     destinyId = checkParams('destinyId'),
     memberType = checkParams('memberType'),
