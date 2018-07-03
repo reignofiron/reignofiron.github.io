2320149eede5d6114921d8391cd3f304e4e06a22
Jesse Rogers <jesse@jesserogers.net>
2018-07-03 10:19:38 -0700

    Added videos

--- _site/assets/js/player.js
+++ _site/assets/js/player.js
@@ -2,7 +2,7 @@ $(function() {
 
   var
     apiKey = "39424dade4d141af9a0807725a14ed20", // production
-// apiKey = "6987280b74b24575a4e805277bb5baa6", // local
+apiKey = "6987280b74b24575a4e805277bb5baa6", // local
 groupID = "2974952"
 ,
     honored = [
